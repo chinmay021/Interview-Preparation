46a2731646f8c3ad1e43665945088baca40a5d79
chinmay021 <chinmaykumar021@gmail.com>
2020-07-31 15:35:09 +0530

    celebrity problem

--- stack/stack(intermediate)/find celebrity problem leetcode.cpp
+++ stack/stack(intermediate)/find celebrity problem leetcode.cpp
@@ -17,3 +17,27 @@ int findCelebrity(int n)
     }
     return candidate;
 }
+// if data is given as a matrix
+bool knows(int a, int b, int M[MAX][MAX])
+{
+    return M[a][b];
+}
+int getId(int M[MAX][MAX], int n)
+{
+    int candidate = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        if (knows(candidate, i, M))
+        {
+            candidate = i;
+        }
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        if (i != candidate && (knows(candidate, i, M) || !knows(i, candidate, M)))
+        {
+            return -1;
+        }
+    }
+    return candidate;
+}
