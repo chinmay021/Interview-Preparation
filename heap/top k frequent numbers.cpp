985e58f5631ff126f46ea999fd9474ba5aaee8de
chinmay021 <chinmaykumar021@gmail.com>
2020-08-12 19:20:30 +0530

    top k frequent numbers

---
+++ heap/top k frequent numbers.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        vector<int> result;
+        if (nums.size() <= 1)
+        {
+            return nums;
+        }
+        unordered_map<int, int> mp;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minh;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            mp[nums[i]]++;
+        }
+        for (auto it = mp.begin(); it != mp.end(); it++)
+        {
+            minh.push({it->second, it->first});
+            if (minh.size() > k)
+                minh.pop();
+        }
+        while (minh.size() > 0)
+        {
+            result.push_back(minh.top().second);
+            minh.pop();
+        }
+        return result;
+    }
+};
