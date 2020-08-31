b6c563d5868ec934908b550be75ea5cef7829963
chinmay021 <chinmaykumar021@gmail.com>
2020-08-31 10:56:24 +0530

    kth symbol in grammar

---
+++ recursion/kth symbol in grammar.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int kthGrammar(int N, int K)
+    {
+        if (N == 1 && K == 1)
+        {
+            return 0;
+        }
+        int length = pow(2, N - 1);
+        int mid = length / 2;
+        if (K <= mid)
+        {
+            return kthGrammar(N - 1, K);
+        }
+        else
+        {
+            return !kthGrammar(N - 1, K - mid);
+        }
+    }
+};
